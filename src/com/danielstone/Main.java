c19f192966dbac78760a5d4a10ad5fb463014dee
daniel-stoneuk <danielstone411@gmail.com>
2016-01-05 19:38:34 +0000

    JavaFX GUI - 4 - Switching Scenes

--- src/com/danielstone/Main.java
+++ src/com/danielstone/Main.java
@@ -3,12 +3,15 @@ package com.danielstone;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application{
 
-    Button button;
+    Stage window;
+    Scene scene1, scene2;
 
     public static void main(String[] args) {
         launch(args);
@@ -16,34 +19,33 @@ public class Main extends Application{
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("Title of the window");
+        window = primaryStage;
 
+        Label label1 = new Label("Welcome to the first scene!");
 
-        button = new Button();
-        button.setText("Click me");
+        //Button 1
+        Button button1 = new Button("Go to scene 2");
+        button1.setOnAction(event -> window.setScene(scene2));
 
-        //LAMBDA
-        button.setOnAction(e -> {
-            System.out.println("HEY NOW");
-        });
+        //Layout 1 - children are laid out in a vertical column
+        VBox layout1 = new VBox(20);
+        layout1.getChildren().addAll(label1, button1);
 
-        /*
-        *ANONYMOUS INNER CLASS
-        button.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("I am anonymous inner class");
-            }
-        });
-        */
+        scene1 = new Scene(layout1, 200f, 200f);
 
-        StackPane layout = new StackPane();
-        layout.getChildren().add(button);
+        //Button 2
+        Button button2 = new Button("This scene sucks, go back to scene 1");
+        button2.setOnAction(event -> window.setScene(scene1));
 
-        Scene scene = new Scene(layout, 500, 400);
-        primaryStage.setScene(scene);
+        //Layout 2 -
+        StackPane layout2 = new StackPane();
+        layout2.getChildren().add(button2);
+        scene2 = new Scene(layout2, 600, 300);
 
-        primaryStage.show();
+        window.setScene(scene1);
+        window.setTitle("Title");
+        window.show();
+
     }
 
 }
