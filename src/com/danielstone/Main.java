752376b9d8ed4cf3b05888c6b4b2bb2bffcc35c5
daniel-stoneuk <danielstone411@gmail.com>
2016-01-05 19:50:42 +0000

    JavaFX GUI - 5 - Alert box - modals ets

--- src/com/danielstone/Main.java
+++ src/com/danielstone/Main.java
@@ -5,13 +5,12 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application{
 
     Stage window;
-    Scene scene1, scene2;
+    Scene scene;
 
     public static void main(String[] args) {
         launch(args);
@@ -23,26 +22,17 @@ public class Main extends Application{
 
         Label label1 = new Label("Welcome to the first scene!");
 
+
         //Button 1
-        Button button1 = new Button("Go to scene 2");
-        button1.setOnAction(event -> window.setScene(scene2));
-
-        //Layout 1 - children are laid out in a vertical column
-        VBox layout1 = new VBox(20);
-        layout1.getChildren().addAll(label1, button1);
-
-        scene1 = new Scene(layout1, 200f, 200f);
-
-        //Button 2
-        Button button2 = new Button("This scene sucks, go back to scene 1");
-        button2.setOnAction(event -> window.setScene(scene1));
+        Button button = new Button("Click Me");
+        button.setOnAction(event -> AlertBox.display("Alert", "Example Alert"));
 
         //Layout 2 -
-        StackPane layout2 = new StackPane();
-        layout2.getChildren().add(button2);
-        scene2 = new Scene(layout2, 600, 300);
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+        scene = new Scene(layout, 600, 300);
 
-        window.setScene(scene1);
+        window.setScene(scene);
         window.setTitle("Title");
         window.show();
 
