b48265f3c199f92f3fc86f0db07703bf2ef91a57
daniel-stoneuk <danielstone411@gmail.com>
2016-01-05 19:19:58 +0000

    JavaFX GUI Tutorial - 2

--- src/com/danielstone/Main.java
+++ src/com/danielstone/Main.java
@@ -1,10 +1,14 @@
 package com.danielstone;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class Main extends Application implements EventHandler<ActionEvent> {
 
     Button button;
 
@@ -20,6 +24,21 @@ public class Main extends Application {
         button = new Button();
         button.setText("Click me");
 
+        button.setOnAction(this);
 
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+
+        Scene scene = new Scene(layout, 500, 400);
+        primaryStage.setScene(scene);
+
+        primaryStage.show();
+    }
+
+    @Override
+    public void handle(ActionEvent event) {
+        if (event.getSource() == button) {
+            System.out.println("Button Pressed...");
+        }
     }
 }
